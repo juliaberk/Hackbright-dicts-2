89ec55c2bf333f6dbd2491e0683165469b31ff63
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 16:33:25 -0800

    added sorted list functionality and user input

--- ratings.py
+++ ratings.py
@@ -1,8 +1,17 @@
 """Restaurant rating lister."""
 from sys import argv
 
+
 # put your code here
 def sorts_restaurants(filename):
+    """Prints alphabetical list of restaurants and ratings. Allows user input.
+
+        Allows multiple arguments for filenames
+        Files are expected in restaurant:rating format
+
+        Prints strings of restaurants and their ratings.
+    """
+    
     with open(filename) as f:
 
         restaurant_ratings = {}
@@ -14,8 +23,22 @@ def sorts_restaurants(filename):
             name, rating = entry.split(':')
             restaurant_ratings[name] = rating
 
-        for name, rating in restaurant_ratings.iteritems():
-            print name + " is rated at " + rating +"."
+        new_restaurant = raw_input("What is the restaurant name? ")
+        new_rating = raw_input("What is its rating? ")
+
+        while True:
+            if 0 < int(new_rating) < 6:
+                break
+            else:
+                new_rating = int(raw_input("Enter a rating from 1 to 5. "))
+
+        restaurant_ratings[new_restaurant] = new_rating
+
+        rest_ratings_list = restaurant_ratings.items()
+        sorted_restaurants = sorted(rest_ratings_list)
+
+        for name, rating in sorted_restaurants:
+            print name.title() + " is rated at " + str(rating) + "."
 
 for arg in argv[1:]:
     sorts_restaurants(arg)
